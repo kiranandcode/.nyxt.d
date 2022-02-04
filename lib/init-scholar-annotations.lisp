73c5534b5812aeb61f3ad4554a3f84ec9e61f29b
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2022-02-04 13:11:09 +0000

    removed commented out legacy snippet

--- lib/init-scholar-annotations.lisp
+++ lib/init-scholar-annotations.lisp
@@ -116,19 +116,3 @@ id to THEN."
   ((nyxt:buffer-loaded-hook
     (hooks:add-hook %slot-default% #'update-scholar-entries-with-annotations-handler)
     )))
-
-;; (defun old-reddit-handler (request-data)
-;;   (let ((url (url request-data)))
-;;     (setf (url request-data)
-;;           (if (search "reddit.com" (quri:uri-host url))
-;;               (progn
-;;                 (setf (quri:uri-host url) "old.reddit.com")
-;;                 (log:info "Switching to old Reddit: ~s" (object-display url))
-;;                 url)
-;;               url)))
-;;   request-data)
-;; (use-package :nyxt)
-
-;; (define-configuration web-buffer
-;;   ((request-resource-hook
-;;     (hooks:add-hook %slot-default (make-handler-resource #'old-reddit-handler)))))
